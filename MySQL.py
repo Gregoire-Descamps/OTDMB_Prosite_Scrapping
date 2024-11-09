e425dc5f9e1ca24730dbea8ead16516ad06e8f85
gregoire-descamps <gregoire.descamps@lacatholille.fr>
2024-11-09 20:04:24 +0100

    WSL Auto install Test tried to implement the auto installing through wsl but for now nothing is working...

--- MySQL.py
+++ MySQL.py
@@ -1,1 +1,7 @@
-import d
+import sqlalchemy, sqlalchemy_utils as sqlu
+
+def CreateEngine( engineURL= None):
+    if engineURL == None :
+        engine = sqlalchemy.create_engine("postgresql://localhost/mydb")
+        if not sqlu.database_exists(engine.url):
+            sqlu.create_database(engine.url)
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 #allow to install automatically required pakages at startup
 import subprocess, sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+try :
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+except :
+
+
+    subprocess.Popen(["sudo", "apt", "install", "python3-venv"], shell=True)
+    subprocess.check_call(["wsl.exe", "source", "env/bin/activate"], shell=False)
+    subprocess.check_call(["-m", "pip", "install", "-r", "requirements.txt"], shell=False)
+
+
+import MySQL
+
+MySQL.CreateEngine("MySQL://localhost/PrositeLocalDB")
 
