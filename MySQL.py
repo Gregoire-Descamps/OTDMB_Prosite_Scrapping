509303ca35e3781176db385344fb89444b1033e2
gregoire-descamps <gregoire.descamps@lacatholille.fr>
2024-11-14 06:36:11 +0100

    Minor changes and added comments

--- MySQL.py
+++ MySQL.py
@@ -10,6 +10,7 @@ import pymysql
 class PrositeBase(DeclarativeBase):
     pass
 
+# The Entry table model
 class Entry(PrositeBase):
     __tablename__ = "Entry"
 
@@ -18,7 +19,7 @@ class Entry(PrositeBase):
     entry_type : Mapped[str] = mapped_column(String(7))
     Description : Mapped["Description"] = relationship(back_populates="Entry")
 
-
+# The Description table model
 class Description(PrositeBase):
     __tablename__ = "Description"
 
@@ -26,12 +27,13 @@ class Description(PrositeBase):
     description: Mapped[str] = mapped_column(String(10000))
 
 
-
-
-
-
 # A DataBase Connection
+# Initialize a connection to the DataBase prompting for user credentials.
+# If the DB doesn't exist, automatically generate one.
 class DBConnection:
+
+    # Password is retrieved using getpass.
+    # It allows to hide the user typed characters but require access to the terminal output
     def __init__(self):
         self._username = input("Enter your DataBase User Name: ")
         print(f"Let's connect {self._username}!\n(If you're on IDE and the code stop running from here, make sure you enabled terminal emulation!")
@@ -43,24 +45,32 @@ class DBConnection:
     def __URL__(self):
         return f"mysql+pymysql://{self._username}:{self.__psw}@localhost/PrositeLocalDB"
 
-# Create the DB engine to connect to a DB
+    # Create the DB engine to connect to a DB
     def CreateEngine(self):
+
+        # Check for DB, if it doesn't exist, create one
         if not database_exists(self.__URL__()):
             self.__CreateDB__()
 
         return create_engine(self.__URL__())
 
 
-    # Create the database
+    # Create the database.
+    # Use temporary connection engine and PrositeBase() defined Class and childs to create the DB
     def __CreateDB__(self):
         create_database(self.__URL__())
         tempEngine = create_engine(self.__URL__())
         thisDB = PrositeBase()
         thisDB.metadata.create_all(tempEngine)
 
-
+    # "Commit as you go" method
     def con(self):
         return self.engine.connect()
 
+    # "Begin once" method
     def begin(self):
         return self.engine.begin()
+
+    # Session method, allow to initialize a session and instantiate entry objects for the DB
+    def Session(self):
+        return Session(self.engine)
